c85010bceec934650ce31ac7cf9837d3a3ab7cb7
Matt Skinner <matthewskin@gmail.com>
2021-03-31 12:24:39 -0500

    Added some bullshit.

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ dotenv.config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+let messageNathanInterval;
+
+client.login(process.env.BOT_TOKEN);
+
 client.once('ready', () => {
 	console.log('Ready!');
 });
@@ -17,30 +21,64 @@ client.on('message', message => {
 
 		if (message.content === '!ping') {
 			switch (message.author.tag) {
-			case process.env.JESIKA_TAG:
-				message.channel.send('No girls allowed.');
-				break;
-			case process.env.JARED_TAG:
-				message.channel.send('poop');
-				break;
-			case process.env.MATT_TAG:
-				message.channel.send('pong');
-				break;
-			case process.env.NATHAN_TAG:
-				message.channel.send('Wtf');
-				break;
-			case process.env.CHASE_TAG:
-				message.channel.send('It\'s not gay if you\'re underway.');
-				break;
-			default:
-				message.channel.send('pong');
+				case process.env.JESIKA_TAG:
+					message.channel.send('No girls allowed.');
+					break;
+				case process.env.JARED_TAG:
+					message.channel.send('poop');
+					break;
+				case process.env.MATT_TAG:
+					message.channel.send('pong');
+					break;
+				case process.env.NATHAN_TAG:
+					message.channel.send('Wtf');
+					break;
+				case process.env.CHASE_TAG:
+					message.channel.send('It\'s not gay if you\'re underway.');
+					break;
+				default:
+					message.channel.send('pong');
 			}
+		}
+
+		if (message.content === '!ching') {
+			message.channel.send('chong');
+		}
+
+		if (message.content === '!bing') {
+			message.channel.send('bong');
+		}
+
+		if (message.content === '!listen') {
+			messageNathanInterval = setInterval(messageNathan, 3000);
+		}
+
+		if (message.content === '!stop') {
+			if(messageNathanInterval) {
+				clearInterval(messageNathanInterval);
+			}
+
+			messageNathanInterval = null;
+		}
+
+		if (message.content === '!wormhole') {
+			const apiMessage = new Discord.APIMessage(message.channel, {
+				content: '<@127296623779774464> wormhole send poop',
+				disableMentions: 'none',
+			});
+			message.channel.send(apiMessage);
 		}
 	}
 });
 
-client.login(process.env.BOT_TOKEN);
-
 function isValidUser(author) {
 	return !author.bot;
+}
+
+function messageNathan() {
+	const apiMessage = new Discord.APIMessage(client.channels.cache.get('234762591107284992'), {
+		content: '<@235548090050805770> Nathan! Listen!',
+		disableMentions: 'none',
+	});
+	client.channels.cache.get('234762591107284992').send(apiMessage);
 }
